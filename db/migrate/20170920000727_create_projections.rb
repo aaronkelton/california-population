9633c9a6e114caad30df7883bebe0ae529143b8a
chemturion <aaron.k.kelton@gmail.com>
2017-09-19 19:25:47 -0500

    wrote columns in migration

--- db/migrate/20170920000727_create_projections.rb
+++ db/migrate/20170920000727_create_projections.rb
@@ -1,6 +1,14 @@
 class CreateProjections < ActiveRecord::Migration[5.1]
   def change
     create_table :projections do |t|
+      t.string  :county_code, limit: 10, null: false # converts to VARCHAR(10) and NOT NULL
+      t.string  :county,      limit: 45, null: false
+      t.integer :date_year,              null: false
+      t.integer :race_code,              null: false
+      t.text    :race,                   null: false
+      t.string  :gender,      limit: 6,  null: false
+      t.integer :age,                    null: false
+      t.ingeger :population,             null: false
 
       t.timestamps
     end
